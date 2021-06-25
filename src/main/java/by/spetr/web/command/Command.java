ba97b40333cdb2ce4fcee807498599afca673f83
alexridvansky <alexridvansky@gmail.com>
2021-06-25 16:42:02 +0300

    @FunctionalInterface annotation added

--- src/main/java/by/spetr/web/command/Command.java
+++ src/main/java/by/spetr/web/command/Command.java
@@ -2,8 +2,8 @@ package by.spetr.web.command;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+@FunctionalInterface
 public interface Command {
-
     /*
      * @param {@code HttpServletRequest} request
      * @return {@code String} forwarding or redirecting path
