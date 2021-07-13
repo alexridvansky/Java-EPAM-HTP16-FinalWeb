f01d5408ad8e3bb79c578991d22b3c3898d50ab3
alexridvansky <alexridvansky@gmail.com>
2021-07-13 18:44:05 +0300

    Controller

---
+++ src/main/java/by/spetr/web/controller/Controller.java
@@ -0,0 +1,95 @@
+package by.spetr.web.controller;
+
+import by.spetr.web.command.*;
+import by.spetr.web.model.exception.ConnectionPoolException;
+import by.spetr.web.model.pool.ConnectionPool;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+
+/**
+ * class {@code Controller} is used for processing requests matching '*.do' pattern
+ */
+@WebServlet(name = "controller", urlPatterns = {"/controller", "*.do"})
+public class Controller extends HttpServlet {
+    private static final Logger logger = LogManager.getLogger();
+
+    /**
+     * Redirects {@code doGet} requests toward {@code Controller Servlet}
+     *
+     * @param request - {@code HttpServletRequest} request
+     * @param response - {@code HttpServletResponse} response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.info("Controller: doGet is being called, redirecting...");
+        processRequest(request, response);
+    }
+
+    /**
+     * Redirects {@code doPost} requests toward {@code Controller Servlet}
+     *
+     * @param request - {@code HttpServletRequest} request
+     * @param response - {@code HttpServletResponse} response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.info("Controller: doPost is being called, redirecting...");
+        processRequest(request, response);
+    }
+
+    /**
+     * Processes redirected doGet and doPost requests
+     *
+     * @param request - {@code HttpServletRequest} request
+     * @param response - {@code HttpServletResponse} response
+     * @throws ServletException – if the target resource throws this exception
+     * @throws IOException – if the target resource throws this exception
+     */
+    private <Users> void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws IOException, ServletException {
+        logger.debug("New request (Url: {})",
+                request.getRequestURL() + (request.getQueryString() != null
+                        ? "?" + request.getQueryString()
+                        : ""));
+
+        String commandName = request.getParameter(RequestParameter.COMMAND);
+        logger.debug("commandName: {}", commandName);
+        Command command = CommandProvider.getInstance().getCommand(commandName);
+        Router router = command.execute(request);
+
+        if (router.getRouterType() == Router.RouterType.REDIRECT) {
+            response.sendRedirect(router.getPagePath());
+        } else if (router.getRouterType() == Router.RouterType.FORWARD) {
+            RequestDispatcher dispatcher = request.getRequestDispatcher(router.getPagePath());
+            dispatcher.forward(request, response);
+        } else {
+            logger.error("Incorrect router type: {}", router.getRouterType());
+            response.sendRedirect(PagePathConstant.ERROR_PAGE);
+            // todo: ASK! in case if router is any other type to throw some servletException OR redirect to error_page?
+        }
+    }
+
+    /**
+     * Destroys {@code Servlet} and {@code ConnectionPool}
+     */
+    @Override
+    public void destroy() {
+        try {
+            logger.info("pool.destroy()");
+            ConnectionPool.getInstance().destroyPool();
+        } catch (ConnectionPoolException e) {
+            logger.error("Error destroying ConnectionPool", e);
+        }
+        super.destroy();
+    }
+}
---
+++ src/main/java/by/spetr/web/controller/filter/SecurityFilter.java
@@ -0,0 +1,38 @@
+package by.spetr.web.controller.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"*.do"})
+public class SecurityFilter implements Filter {
+    private static final Logger logger = LogManager.getLogger();
+    private static final String COMMAND = "command";
+
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        logger.info("SecurityFilter...");
+        HttpServletRequest servletRequest = (HttpServletRequest) request;
+        HttpServletResponse servletResponse = (HttpServletResponse) response;
+        HttpSession session = servletRequest.getSession();
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
