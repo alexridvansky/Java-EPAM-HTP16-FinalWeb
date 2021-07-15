023524c4c49646c56b22c4a188bd52a72d0b447a
alexridvansky <alexridvansky@gmail.com>
2021-07-15 19:42:46 +0300

    RequestParameter - new parameters added

--- src/main/java/by/spetr/web/command/RequestParameter.java
+++ src/main/java/by/spetr/web/command/RequestParameter.java
@@ -2,4 +2,13 @@ package by.spetr.web.command;
 
 public final class RequestParameter {
     public static final String COMMAND = "command";
+    public static final String USER_LIST_PARAM = "users";
+    public static final String USER_PARAM = "user";
+    public static final String USERNAME_PARAM = "username";
+    public static final String PASSWORD_PARAM = "password";
+    public static final String PASSWORD_AGAIN_PARAM = "password_again";
+    public static final String EMAIL_PARAM = "email";
+    public static final String PHONE_PARAM = "phone";
+    public static final String REGISTRATION_DATA = "reg_data";
+    public static final String AUTHENTICATION_ERROR = "auth_error";
 }
