25cbe45fb3b25604d3480426567f4f6ed2307021
alexridvansky <alexridvansky@gmail.com>
2021-07-27 01:10:29 +0300

    ShowUserListCommand, ShowVehicleAdsCommand
    code added

--- src/main/java/by/spetr/web/command/user/ShowUserListCommand.java
+++ src/main/java/by/spetr/web/command/user/ShowUserListCommand.java
@@ -9,17 +9,25 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.List;
 
-public class PrintAllUsersCommand implements Command {
+import static by.spetr.web.command.PagePath.SHOW_USER_LIST;
+import static by.spetr.web.command.RequestParameter.LAST_PAGE_PARAM;
+
+public class ShowUserListCommand implements Command {
 
     @Override
     public Router execute(HttpServletRequest request) {
         UserService userService = DefaultUserService.getInstance();
+
         try {
             List<User> users = userService.getUserList();
             request.setAttribute(RequestParameter.USER_LIST_PARAM, users);
-            return new Router(PagePath.PRINT_ALL_USERS, Router.RouterType.FORWARD);
+            request.getSession().setAttribute(LAST_PAGE_PARAM, SHOW_USER_LIST);
+
+            return new Router(SHOW_USER_LIST);
+
         } catch (ServiceException e) {
             // todo: go to some showing_user_error.page
+
             return new Router(PagePath.ERROR_PAGE, Router.RouterType.REDIRECT);
         }
     }
--- src/main/java/by/spetr/web/command/vehicle/ShowVehicleAdsCommand.java
+++ src/main/java/by/spetr/web/command/vehicle/ShowVehicleAdsCommand.java
@@ -1,7 +1,8 @@
-package by.spetr.web.command.user;
+package by.spetr.web.command.vehicle;
 
 import by.spetr.web.command.Command;
 import by.spetr.web.command.Router;
+import by.spetr.web.model.dto.VehiclePreviewDto;
 import by.spetr.web.model.entity.Vehicle;
 import by.spetr.web.model.exception.ServiceException;
 import by.spetr.web.model.service.DefaultVehicleService;
@@ -10,26 +11,25 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import java.util.List;
 
-import static by.spetr.web.command.PagePath.SHOW_VEHICLE_ADS;
+import static by.spetr.web.command.PagePath.*;
 import static by.spetr.web.command.RequestParameter.LAST_PAGE_PARAM;
 import static by.spetr.web.command.RequestParameter.VEHICLE_LIST_PARAM;
 
-public class ShowVehicleAds implements Command {
+public class ShowVehicleAdsCommand implements Command {
     private static final VehicleService vehicleService = DefaultVehicleService.getInstance();
 
     @Override
     public Router execute(HttpServletRequest request) {
         try {
-            List<Vehicle> vehicles = vehicleService.getVehicleList();
+            List<VehiclePreviewDto> vehicles = vehicleService.getPublicVehicleList();
+
             request.getSession().setAttribute(LAST_PAGE_PARAM, SHOW_VEHICLE_ADS);
             request.setAttribute(VEHICLE_LIST_PARAM, vehicles);
 
             return new Router(SHOW_VEHICLE_ADS);
 
         } catch (ServiceException e) {
-            e.printStackTrace();
+            return new Router(ERROR_PAGE);
         }
-
-        return null;
     }
 }
