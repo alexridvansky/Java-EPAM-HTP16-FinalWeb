eefb8f1026f8fb7634fd67290983e5f3c18f3ae2
alexridvansky <alexridvansky@gmail.com>
2021-07-27 01:09:30 +0300

    ServiceMessageList
    class added

---
+++ src/main/java/by/spetr/web/model/service/ServiceMessageList.java
@@ -0,0 +1,16 @@
+package by.spetr.web.model.service;
+
+public class ServiceMessageList {
+    private ServiceMessageList(){}
+
+    public static final String USERNAME_INVALID = "Username doesn't match requirements";
+    public static final String USERNAME_TAKEN = "Username is already taken";
+    public static final String PASSWORDS_DIFFERENT = "Entered passwords are different";
+    public static final String PASSWORD_INVALID = "Password doesn't match requirements";
+    public static final String EMAIL_INVALID = "E-Mail doesn't match requirements";
+    public static final String EMAIL_TAKEN = "E-Mail is already taken";
+    public static final String PHONE_INVALID = "Phone number doesn't match requirements";
+    public static final String PHONE_TAKEN = "Phone number is already taken";
+    public static final String DAO_ERROR = "Error occurred on DAO layer";
+    public static final String CLOUDINARY_UPLOAD_ERROR = "Error on Cloudinary service. File(s) can't be uploaded";
+}
