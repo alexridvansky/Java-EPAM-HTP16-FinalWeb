678207e14958daa89a79d1c9d8fbef266ac20026
alexridvansky <alexridvansky@gmail.com>
2021-07-26 19:39:48 +0300

    ChangeLocaleCommand
    command body added

--- src/main/java/by/spetr/web/command/user/ChangeLocaleCommand.java
+++ src/main/java/by/spetr/web/command/user/ChangeLocaleCommand.java
@@ -8,13 +8,25 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.Locale;
 
+import static by.spetr.web.command.PagePath.INDEX_PAGE;
+import static by.spetr.web.command.RequestParameter.*;
+
 public class ChangeLocaleCommand implements Command {
     private static final Logger logger = LogManager.getLogger();
 
     @Override
     public Router execute(HttpServletRequest request) {
-        Locale oldLocale = request.getLocale();
+        Locale locale = request.getLocale();
+        String newLocale = request.getParameter(NEW_LOCALE_PARAM);
+        request.getSession().setAttribute(LOCALE_PARAM, newLocale);
+        logger.debug("locale: {}", locale);
+        logger.debug("new requestet locale: {}", newLocale);
 
-        return null; // todo:
+        String lastPage = (String) request.getSession().getAttribute(LAST_PAGE_PARAM);
+        if (lastPage == null) {
+            lastPage = INDEX_PAGE;
+        }
+
+        return new Router(lastPage);
     }
 }
