a636f888fdbf4ecb4c69422c3cfb017a00644718
alexridvansky <alexridvansky@gmail.com>
2021-07-27 01:04:43 +0300

    Message
    added Serializable implementation

--- src/main/java/by/spetr/web/model/entity/Message.java
+++ src/main/java/by/spetr/web/model/entity/Message.java
@@ -1,8 +1,9 @@
 package by.spetr.web.model.entity;
 
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
-public class Message extends AbstractEntity {
+public class Message extends AbstractEntity implements Serializable {
     private long messageId;
     private String topic;
     private String message;
