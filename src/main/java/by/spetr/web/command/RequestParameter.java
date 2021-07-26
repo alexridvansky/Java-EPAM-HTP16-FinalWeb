4db11b654a9a20e2b0a1bfab35c36d4e864d2c08
alexridvansky <alexridvansky@gmail.com>
2021-07-27 01:21:35 +0300

    RequestParameter
    values added

--- src/main/java/by/spetr/web/command/RequestParameter.java
+++ src/main/java/by/spetr/web/command/RequestParameter.java
@@ -1,14 +1,28 @@
 package by.spetr.web.command;
 
 public final class RequestParameter {
+    private RequestParameter() {}
+
     public static final String COMMAND = "command";
     public static final String USER_LIST_PARAM = "users";
     public static final String USER_PARAM = "user";
-    public static final String USERNAME_PARAM = "username";
+    public static final String USER_NAME_PARAM = "username";
+    public static final String USER_STATE_PARAM = "new_state";
+    public static final String USER_ROLE_PARAM = "new_role";
+    public static final String VEHICLE_LIST_PARAM = "vehicles";
+    public static final String VEHICLE_PARAM = "vehicle";
+    public static final String VEHICLE_ID_PARAM = "vehicle_id";
+    public static final String VEHICLE_STATE_PARAM = "vehicle_state";
     public static final String PASSWORD_PARAM = "password";
     public static final String PASSWORD_AGAIN_PARAM = "password_again";
     public static final String EMAIL_PARAM = "email";
     public static final String PHONE_PARAM = "phone";
     public static final String REGISTRATION_DATA = "reg_data";
     public static final String AUTHENTICATION_ERROR = "auth_error";
+    public static final String LAST_PAGE_PARAM = "last_page";
+    public static final String LOCALE_PARAM = "locale";
+    public static final String NEW_LOCALE_PARAM = "new_locale";
+    public static final String FILENAME_PARAM = "filename";
+    public static final String MESSAGE_PARAM = "message";
+    public static final String PREVIEW_PARAM = "preview";
 }
