ce06ec47e0518ffee91cfe24ccc95d38339ddeb2
alexridvansky <alexridvansky@gmail.com>
2021-07-27 00:56:27 +0300

    ColumnName
    class, contains names of column added

---
+++ src/main/java/by/spetr/web/model/dao/ColumnName.java
@@ -0,0 +1,19 @@
+package by.spetr.web.model.dao;
+
+public class ColumnName {
+    private ColumnName() {}
+
+    public static final String VEHICLE_ID = "vehicle_id";
+    public static final String IMAGE_PATH = "img_path";
+    public static final String VEHICLE_OWNER_LOGIN = "login";
+    public static final String VEHICLE_MAKE = "make";
+    public static final String VEHICLE_MODEL = "model";
+    public static final String VEHICLE_MODEL_YEAR = "modelyear";
+    public static final String VEHICLE_PRICE = "price";
+    public static final String VEHICLE_POWERTRAIN = "powertrain";
+    public static final String VEHICLE_TRANSMISSION = "transmission";
+    public static final String VEHICLE_DRIVE = "drive";
+    public static final String VEHICLE_DISPLACEMENT = "displacement";
+    public static final String VEHICLE_POWER = "power";
+    public static final String VEHICLE_CREATION_DATE = "creation_date";
+}
