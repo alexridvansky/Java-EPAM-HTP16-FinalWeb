1aef127725f047cb74d753b8a571ef51ca5643d1
alexridvansky <alexridvansky@gmail.com>
2021-07-27 01:54:36 +0300

    PagePath
    class added

---
+++ src/main/java/by/spetr/web/command/PagePath.java
@@ -0,0 +1,23 @@
+package by.spetr.web.command;
+
+public class PagePath {
+    public static final String SIGN_UP_PAGE = "/jsp/sign_up.jsp";
+    public static final String SIGN_UP_RESULT_PAGE = "/jsp/sign_up_result.jsp";
+    public static final String SHOW_USER_LIST = "/jsp/show_user_list.jsp";
+    public static final String SHOW_VEHICLE_LIST = "/jsp/show_vehicle_list.jsp";
+    public static final String SHOW_VEHICLE_INFO = "/jsp/show_vehicle_info.jsp";
+    public static final String SHOW_VEHICLE_NOT_FOUND = "/jsp/show_vehicle_not_found.jsp";
+    public static final String SHOW_VEHICLE_ADS = "/jsp/show_vehicle_ads.jsp";
+    public static final String ERROR_404_PAGE = "/jsp/error_404.jsp";
+    public static final String ERROR_PAGE = "/jsp/error.jsp";
+    public static final String INDEX_PAGE = "index.jsp";
+    public static final String MAIN_PAGE = "/jsp/main.jsp";
+    public static final String LOGIN_ERROR = "/jsp/sign_in_error.jsp";
+    public static final String UNSUPPORTED_COMMAND = "/jsp/error/command_error.jsp";
+
+
+
+    private PagePath() {
+    }
+
+}
