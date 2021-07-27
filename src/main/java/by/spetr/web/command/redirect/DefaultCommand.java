a5a898177a3fb54c10f73e7d6ba5cdce8bb0cbe7
alexridvansky <alexridvansky@gmail.com>
2021-07-27 18:21:23 +0300

    DefaultCommand
    argument (pagePath constant class) renamed

--- src/main/java/by/spetr/web/command/redirect/DefaultCommand.java
+++ src/main/java/by/spetr/web/command/redirect/DefaultCommand.java
@@ -1,14 +1,13 @@
-package by.spetr.web.command.list;
+package by.spetr.web.command.redirect;
 
 import by.spetr.web.command.Command;
 import by.spetr.web.command.Router;
-import by.spetr.web.command.PagePathConstant;
+import by.spetr.web.command.PagePath;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 public class DefaultCommand implements Command {
     @Override
     public Router execute(HttpServletRequest request) {
-        return new Router(PagePathConstant.ERROR_404_PAGE, Router.RouterType.REDIRECT);
+        return new Router(PagePath.ERROR_404_PAGE, Router.RouterType.REDIRECT);
     }
 }
