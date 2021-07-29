f935387d1cf81f763923a20d9a0b1199ddceb093
alexridvansky <alexridvansky@gmail.com>
2021-07-29 12:11:32 +0300

    BCrypt tests
    tests for BCrypt library added

---
+++ src/test/java/by/spetr/web/util/BCryptTest.java
@@ -0,0 +1,37 @@
+package by.spetr.web.util;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class BCryptTest {
+    private static final String PASSWORD_USER_TO_BE_HASHED = "user";
+    private static final String PASSWORD_ROOT_TO_BE_HASHED = "root";
+
+
+    @Test
+    public void testCheckUserPass() {
+        String hashed_pass = BCrypt.hashpw(PASSWORD_USER_TO_BE_HASHED, BCrypt.gensalt());
+        Assert.assertTrue(BCrypt.checkpw(PASSWORD_USER_TO_BE_HASHED,hashed_pass));
+    }
+
+    @Test
+    public void testCheckRootPass() {
+        String hashed_pass = BCrypt.hashpw(PASSWORD_ROOT_TO_BE_HASHED, BCrypt.gensalt());
+        Assert.assertTrue(BCrypt.checkpw(PASSWORD_ROOT_TO_BE_HASHED,hashed_pass));
+    }
+
+    @Test
+    public void testGeneratingUserPass() {
+        String hashed_pass_once = BCrypt.hashpw(PASSWORD_USER_TO_BE_HASHED,BCrypt.gensalt());
+        String hashed_pass_twice = BCrypt.hashpw(PASSWORD_USER_TO_BE_HASHED,BCrypt.gensalt());
+        Assert.assertNotEquals(hashed_pass_once, hashed_pass_twice);
+    }
+
+    @Test
+    public void testGeneratingRootPass() {
+        String hashed_pass_once = BCrypt.hashpw(PASSWORD_ROOT_TO_BE_HASHED,BCrypt.gensalt());
+        String hashed_pass_twice = BCrypt.hashpw(PASSWORD_ROOT_TO_BE_HASHED,BCrypt.gensalt());
+        Assert.assertNotEquals(hashed_pass_once, hashed_pass_twice);
+    }
+}
