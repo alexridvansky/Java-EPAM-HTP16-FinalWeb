086056d56a4bc4ce7b571398e495eff029aeba84
alexridvansky <alexridvansky@gmail.com>
2021-08-04 07:14:18 +0300

    VehicleDto, VehicleFullDto
    classes refurbished

--- src/main/java/by/spetr/web/model/dto/VehicleFullDto.java
+++ src/main/java/by/spetr/web/model/dto/VehicleFullDto.java
@@ -2,23 +2,50 @@ package by.spetr.web.model.dto;
 
 import by.spetr.web.model.entity.type.VehicleDriveType;
 import by.spetr.web.model.entity.type.VehiclePowertrainType;
+import by.spetr.web.model.entity.type.VehicleStateType;
 import by.spetr.web.model.entity.type.VehicleTransmissionType;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.Year;
+import java.util.List;
+import java.util.Map;
 
-public class VehicleDto implements Serializable {
+public class VehicleFullDto implements Serializable {
+    private List<String> album;
+    private Map<Long, String> optionsMap;
     private long id;
+    private VehicleStateType state;
+    private String owner;
     private String make;
     private String model;
     private Year modelYear;
+    private int mileage;
+    private String color;
     private BigDecimal price;
     private VehiclePowertrainType powertrain;
     private VehicleTransmissionType transmission;
     private VehicleDriveType drive;
     private int displacement;
-    private String previewImagePath;
+    private int power;
+    private LocalDate dateCreated;
+
+    public List<String> getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(List<String> album) {
+        this.album = album;
+    }
+
+    public Map<Long, String> getOptionsMap() {
+        return optionsMap;
+    }
+
+    public void setOptionsMap(Map<Long, String> optionsMap) {
+        this.optionsMap = optionsMap;
+    }
 
     public long getId() {
         return id;
@@ -26,6 +53,22 @@ public class VehicleDto implements Serializable {
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public VehicleStateType getState() {
+        return state;
+    }
+
+    public void setState(VehicleStateType state) {
+        this.state = state;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
     }
 
     public String getMake() {
@@ -50,6 +93,22 @@ public class VehicleDto implements Serializable {
 
     public void setModelYear(Year modelYear) {
         this.modelYear = modelYear;
+    }
+
+    public int getMileage() {
+        return mileage;
+    }
+
+    public void setMileage(int mileage) {
+        this.mileage = mileage;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
     }
 
     public BigDecimal getPrice() {
@@ -92,11 +151,19 @@ public class VehicleDto implements Serializable {
         this.displacement = displacement;
     }
 
-    public String getPreviewImagePath() {
-        return previewImagePath;
+    public int getPower() {
+        return power;
     }
 
-    public void setPreviewImagePath(String previewImagePath) {
-        this.previewImagePath = previewImagePath;
+    public void setPower(int power) {
+        this.power = power;
+    }
+
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
     }
 }
