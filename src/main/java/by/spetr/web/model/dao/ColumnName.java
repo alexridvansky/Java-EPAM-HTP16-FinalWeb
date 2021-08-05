13de9f9a5ecdc718a279ef85e3eff0f2a9404443
alexridvansky <alexridvansky@gmail.com>
2021-08-05 18:01:29 +0300

    ColumnName  values added

--- src/main/java/by/spetr/web/model/dao/ColumnName.java
+++ src/main/java/by/spetr/web/model/dao/ColumnName.java
@@ -1,21 +1,30 @@
 package by.spetr.web.model.dao;
 
-public class ColumnName {
+class ColumnName {
     private ColumnName() {}
 
     public static final String VEHICLE_ID = "vehicle_id";
+    public static final String VEHICLE_STATE = "state";
     public static final String IMAGE_PATH = "img_path";
     public static final String VEHICLE_OWNER_LOGIN = "login";
+    public static final String VEHICLE_MAKE_ID = "vehicle_make_id";
     public static final String VEHICLE_MAKE = "make";
+    public static final String VEHICLE_MODEL_ID = "vehicle_model_id";
     public static final String VEHICLE_MODEL = "model";
     public static final String VEHICLE_MODEL_YEAR = "modelyear";
+    public static final String VEHICLE_MILEAGE = "mileage";
+    public static final String VEHICLE_COLOR = "color";
     public static final String VEHICLE_PRICE = "price";
     public static final String VEHICLE_POWERTRAIN = "powertrain";
     public static final String VEHICLE_TRANSMISSION = "transmission";
     public static final String VEHICLE_DRIVE = "drive";
     public static final String VEHICLE_DISPLACEMENT = "displacement";
     public static final String VEHICLE_POWER = "power";
+    public static final String VEHICLE_COMMENT = "comment";
     public static final String VEHICLE_CREATION_DATE = "creation_date";
     public static final String USER_LOGIN = "login";
     public static final String USER_PASSHASH = "pass";
+    public static final String USER_PHONE = "phone";
+    public static final String OPTION_ID = "option_id";
+    public static final String OPTION_DESCRIPTION = "option_description";
 }
