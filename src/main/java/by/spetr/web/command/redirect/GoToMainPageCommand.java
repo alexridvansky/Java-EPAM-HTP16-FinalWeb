c010fb340403e8e8426db5584c8db6389a69d75a
alexridvansky <alexridvansky@gmail.com>
2021-08-05 18:41:51 +0300

    GoToMainPageCommand, GoToSignUpPageCommand
    minor cosmetic changes

--- src/main/java/by/spetr/web/command/redirect/GoToMainPageCommand.java
+++ src/main/java/by/spetr/web/command/redirect/GoToMainPageCommand.java
@@ -7,24 +7,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import static by.spetr.web.command.PagePath.MAIN_PAGE;
-import static by.spetr.web.command.RequestParameter.LAST_PAGE_PARAM;
-import static by.spetr.web.command.RequestParameter.LOCALE_PARAM;
 
 public class GoToMainPageCommand implements Command {
     private static final Logger logger = LogManager.getLogger();
 
     @Override
     public Router execute(HttpServletRequest request) {
-        if (request.getSession().getAttribute(LOCALE_PARAM) == null) {
-            request.getSession().setAttribute(LOCALE_PARAM, request.getLocale());
-            logger.debug("locale's set by client's system default: {}", request.getLocale());
-        }
+        logger.debug("redirecting to Main page");
 
-        String lastPage = (String) request.getSession().getAttribute(LAST_PAGE_PARAM);
-        logger.debug("redirecting from '{}' to '{}'", lastPage, MAIN_PAGE);
-
-        request.getSession().setAttribute(LAST_PAGE_PARAM,MAIN_PAGE);
-
-        return new Router(MAIN_PAGE, Router.RouterType.REDIRECT);
+        return new Router(MAIN_PAGE);
     }
 }
--- src/main/java/by/spetr/web/command/redirect/GoToSignUpPageCommand.java
+++ src/main/java/by/spetr/web/command/redirect/GoToSignUpPageCommand.java
@@ -7,19 +7,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import static by.spetr.web.command.PagePath.SIGN_UP_PAGE;
-import static by.spetr.web.command.RequestParameter.LAST_PAGE_PARAM;
-
 public class GoToSignUpPageCommand implements Command {
     private static final Logger logger = LogManager.getLogger();
 
     @Override
     public Router execute(HttpServletRequest request) {
-        String lastPage = (String) request.getSession().getAttribute(LAST_PAGE_PARAM);
-        logger.debug("redirecting from '{}' to '{}'", lastPage, SIGN_UP_PAGE);
+        logger.debug("redirect to SignUp page");
 
-        request.getSession().setAttribute(LAST_PAGE_PARAM,SIGN_UP_PAGE);
-
-        return new Router(PagePath.SIGN_UP_PAGE, Router.RouterType.REDIRECT);
+        return new Router(PagePath.SIGN_UP_PAGE);
     }
 }
