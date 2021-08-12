50cc443a534156ae67db2fe8fd1767e365ded5ba
alexridvansky <alexridvansky@gmail.com>
2021-08-12 23:47:18 +0300

    ShowVehicleInfoCommand exception handling added

--- src/main/java/by/spetr/web/controller/command/vehicle/ShowVehicleInfoCommand.java
+++ src/main/java/by/spetr/web/controller/command/vehicle/ShowVehicleInfoCommand.java
@@ -1,22 +1,18 @@
 package by.spetr.web.controller.command.vehicle;
 
 import by.spetr.web.controller.command.Command;
-import by.spetr.web.controller.command.PagePath;
 import by.spetr.web.controller.command.Router;
-
 import by.spetr.web.model.dto.VehicleFullDto;
 import by.spetr.web.model.exception.ServiceException;
 import by.spetr.web.model.service.DefaultVehicleService;
 import by.spetr.web.model.service.VehicleService;
 import jakarta.servlet.http.HttpServletRequest;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Optional;
 
-import static by.spetr.web.controller.command.PagePath.SHOW_VEHICLE_INFO;
-import static by.spetr.web.controller.command.PagePath.SHOW_VEHICLE_NOT_FOUND;
+import static by.spetr.web.controller.command.PagePath.*;
 import static by.spetr.web.controller.command.RequestParameter.*;
 
 public class ShowVehicleInfoCommand implements Command {
@@ -28,18 +24,18 @@ public class ShowVehicleInfoCommand implements Command {
         String vehicleId = request.getParameter(VEHICLE_ID_PARAM);
 
         try {
-            Optional<VehicleFullDto> optionalVehicleFullDto = vehicleService.getFullVehicleInfo(Long.parseLong(vehicleId));
+            Optional<VehicleFullDto> optionalVehicleFullDto = vehicleService.getFullDtoVehicleById(Long.parseLong(vehicleId));
             if (optionalVehicleFullDto.isPresent()) {
                 request.setAttribute(VEHICLE_PARAM, optionalVehicleFullDto.get());
-//                request.getSession().setAttribute(LAST_PAGE_PARAM, SHOW_VEHICLE_INFO);
-
                 return new Router(SHOW_VEHICLE_INFO);
             } else {
                 return new Router(SHOW_VEHICLE_NOT_FOUND);
             }
         } catch (ServiceException e) {
-            // todo: go to some show_user_error.page
-            return new Router(PagePath.ERROR_PAGE);
+            logger.error("Error getting vehicle info from Vehicle.service", e);
+            request.setAttribute(EXCEPTION_MESSAGE, "Error getting vehicle info from Vehicle.service");
+
+            return new Router(ERROR_PAGE);
         }
     }
 }
