1e83ca3b661349b1db20e51ca14b3c028b80c2b0
alexridvansky <alexridvansky@gmail.com>
2021-08-13 00:59:41 +0300

    SignInCommand error handling improved

--- src/main/java/by/spetr/web/controller/command/user/SignInCommand.java
+++ src/main/java/by/spetr/web/controller/command/user/SignInCommand.java
@@ -3,7 +3,7 @@ package by.spetr.web.controller.command.user;
 import by.spetr.web.controller.command.Command;
 import by.spetr.web.controller.command.PagePath;
 import by.spetr.web.controller.command.Router;
-import by.spetr.web.model.entity.User;
+import by.spetr.web.model.dto.UserDto;
 import by.spetr.web.model.exception.ServiceException;
 import by.spetr.web.model.service.DefaultUserService;
 import by.spetr.web.model.service.UserService;
@@ -14,41 +14,35 @@ import org.apache.logging.log4j.Logger;
 import java.util.Objects;
 import java.util.Optional;
 
-import static by.spetr.web.controller.command.PagePath.UNSUPPORTED_COMMAND;
+import static by.spetr.web.controller.command.PagePath.ERROR_PAGE;
 import static by.spetr.web.controller.command.RequestParameter.*;
 
 public class SignInCommand implements Command {
     private static final Logger logger = LogManager.getLogger();
     private static final UserService userService = DefaultUserService.getInstance();
-    private static final String LOGIN_ERROR_MESSAGE = "LOGIN_ERROR";
 
     @Override
     public Router execute(HttpServletRequest request) {
-        User user = (User) request.getSession().getAttribute(USER_PARAM);
         String lastPage = (String) request.getSession().getAttribute(LAST_PAGE_PARAM);
-
-        if (user != null) {
-            logger.error("Attempt to authenticate from already authenticated user");
-            return new Router(UNSUPPORTED_COMMAND, Router.RouterType.REDIRECT);
-        }
 
         String login = request.getParameter("login");
         String pass = request.getParameter("pass");
 
         try {
-            Optional<User> optionalUser = userService.logIn(login, pass);
+            Optional<UserDto> optionalUser = userService.logIn(login, pass);
 
             if (optionalUser.isPresent()) {
                 request.getSession().setAttribute(USER_PARAM, optionalUser.get());
             } else {
-                request.setAttribute(AUTHENTICATION_ERROR, LOGIN_ERROR_MESSAGE);
+                request.setAttribute(AUTHENTICATION_ERROR, "Login error");
             }
-
             return new Router(Objects.requireNonNullElse(lastPage, PagePath.INDEX_PAGE));
 
         } catch (ServiceException e) {
-            e.printStackTrace(); // todo: exc
-            return new Router(PagePath.ERROR_PAGE, Router.RouterType.FORWARD); // todo: exc redirect
+            logger.error(e);
+            request.setAttribute(EXCEPTION_MESSAGE, e.getMessage());
+
+            return new Router(ERROR_PAGE);
         }
     }
 }
