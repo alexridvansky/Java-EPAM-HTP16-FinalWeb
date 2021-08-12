44fc2e24c91e0acdb17f1b9017bf9ce3296681d0
alexridvansky <alexridvansky@gmail.com>
2021-08-13 00:58:04 +0300

    CreateUserCommand autologon after creating user added

--- src/main/java/by/spetr/web/controller/command/user/CreateUserCommand.java
+++ src/main/java/by/spetr/web/controller/command/user/CreateUserCommand.java
@@ -3,7 +3,8 @@ package by.spetr.web.controller.command.user;
 import by.spetr.web.controller.command.Command;
 import by.spetr.web.controller.command.PagePath;
 import by.spetr.web.controller.command.Router;
-import by.spetr.web.model.dto.RegistrationFormDto;
+import by.spetr.web.model.dto.UserDto;
+import by.spetr.web.model.form.UserRegistrationForm;
 import by.spetr.web.model.exception.ServiceException;
 import by.spetr.web.model.service.DefaultUserService;
 import by.spetr.web.model.service.UserService;
@@ -11,17 +12,22 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Objects;
+import java.util.Optional;
+
+import static by.spetr.web.controller.command.PagePath.ERROR_PAGE;
 import static by.spetr.web.controller.command.RequestParameter.*;
 
 public class CreateUserCommand implements Command {
     private static final Logger logger = LogManager.getLogger();
+    private static final UserService userService = DefaultUserService.getInstance();
 
     @Override
     public Router execute(HttpServletRequest request) {
         logger.debug("CreateUserCommand called");
-        UserService userService = DefaultUserService.getInstance();
+        String lastPage = (String) request.getSession().getAttribute(LAST_PAGE_PARAM);
 
-        RegistrationFormDto registrationFormDto = new RegistrationFormDto(
+        UserRegistrationForm userForm = new UserRegistrationForm(
                 request.getParameter(USER_NAME_PARAM),
                 request.getParameter(PASSWORD_PARAM),
                 request.getParameter(PASSWORD_AGAIN_PARAM),
@@ -29,21 +35,22 @@ public class CreateUserCommand implements Command {
                 request.getParameter(PHONE_PARAM)
         );
 
-        boolean isUserCreated = false;
         try {
-            isUserCreated = userService.registerUser(registrationFormDto);
+            Optional<UserDto> optionalUserDto = userService.registerUser(userForm);
+            if (optionalUserDto.isEmpty()) {
+                request.setAttribute(REGISTRATION_DATA, userForm);
+
+                return new Router(PagePath.SIGN_UP_PAGE);
+            } else {
+                request.getSession().setAttribute(USER_PARAM, optionalUserDto.get());
+
+                return new Router(Objects.requireNonNullElse(lastPage, PagePath.INDEX_PAGE));
+            }
         } catch (ServiceException e) {
-            e.printStackTrace(); // todo: exc
+            logger.error(e);
+            request.setAttribute(EXCEPTION_MESSAGE, e.getMessage());
+
+            return new Router(ERROR_PAGE);
         }
-
-        logger.debug("user created: {}", isUserCreated);
-        logger.debug("comment: {}", registrationFormDto.getComment());
-
-        if (!isUserCreated) {
-            request.setAttribute(REGISTRATION_DATA, registrationFormDto);
-            return new Router(PagePath.SIGN_UP_PAGE, Router.RouterType.FORWARD);
-        }
-
-        return new Router(PagePath.SIGN_UP_RESULT_PAGE, Router.RouterType.FORWARD);
     }
 }
