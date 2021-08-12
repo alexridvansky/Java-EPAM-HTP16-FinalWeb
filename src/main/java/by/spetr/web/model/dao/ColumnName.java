fd6c60f3e89956ea4d3af767bcd41baf8b3e3544
alexridvansky <alexridvansky@gmail.com>
2021-08-12 16:41:37 +0300

    ColumnName - fields added

--- src/main/java/by/spetr/web/model/dao/ColumnName.java
+++ src/main/java/by/spetr/web/model/dao/ColumnName.java
@@ -6,6 +6,7 @@ class ColumnName {
     public static final String VEHICLE_ID = "vehicle_id";
     public static final String VEHICLE_STATE = "state";
     public static final String IMAGE_PATH = "img_path";
+    public static final String VEHICLE_OWNER_ID = "owner_id";
     public static final String VEHICLE_OWNER_LOGIN = "login";
     public static final String VEHICLE_MAKE_ID = "vehicle_make_id";
     public static final String VEHICLE_MAKE = "make";
@@ -22,9 +23,13 @@ class ColumnName {
     public static final String VEHICLE_POWER = "power";
     public static final String VEHICLE_COMMENT = "comment";
     public static final String VEHICLE_CREATION_DATE = "creation_date";
+    public static final String VEHICLE_OPTION_ID = "vehicle_option_id";
+    public static final String VEHICLE_POWERTRAIN_ID = "vehicle_powertrain_id";
+    public static final String VEHICLE_COLOR_ID = "vehicle_color_id";
     public static final String USER_LOGIN = "login";
     public static final String USER_PASSHASH = "pass";
     public static final String USER_PHONE = "phone";
     public static final String OPTION_ID = "option_id";
     public static final String OPTION_DESCRIPTION = "option_description";
+    public static final String LAST_INSERT_ID = "last_insert_id()";
 }
