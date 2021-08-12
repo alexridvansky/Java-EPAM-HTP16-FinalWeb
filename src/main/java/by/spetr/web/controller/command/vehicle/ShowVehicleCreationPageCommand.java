0ae388c88780eb37d5bbe08f9f9dd31983015dfc
alexridvansky <alexridvansky@gmail.com>
2021-08-12 22:19:36 +0300

    ShowVehicleCreationPageCommand  code refurbished

--- src/main/java/by/spetr/web/controller/command/vehicle/ShowVehicleCreationPageCommand.java
+++ src/main/java/by/spetr/web/controller/command/vehicle/ShowVehicleCreationPageCommand.java
@@ -3,9 +3,10 @@ package by.spetr.web.controller.command.vehicle;
 import by.spetr.web.controller.command.Command;
 import by.spetr.web.controller.command.PagePath;
 import by.spetr.web.controller.command.Router;
-import by.spetr.web.model.dto.MakeModelListDto;
-import by.spetr.web.model.entity.VehicleMake;
-import by.spetr.web.model.entity.VehicleModel;
+import by.spetr.web.model.entity.*;
+import by.spetr.web.model.entity.type.VehicleDriveType;
+import by.spetr.web.model.entity.type.VehiclePowertrainType;
+import by.spetr.web.model.entity.type.VehicleTransmissionType;
 import by.spetr.web.model.exception.ServiceException;
 import by.spetr.web.model.service.DefaultVehicleService;
 import by.spetr.web.model.service.VehicleService;
@@ -15,13 +16,12 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.List;
 
-import static by.spetr.web.controller.command.PagePath.VEHICLE_CREATION_PAGE;
-import static by.spetr.web.controller.command.RequestParameter.VEHICLE_MAKE_MODEL_LIST;
+import static by.spetr.web.controller.command.PagePath.ADD_VEHICLE_PAGE;
+import static by.spetr.web.controller.command.RequestParameter.*;
 
 
 public class ShowVehicleCreationPageCommand implements Command {
     private static final Logger logger = LogManager.getLogger();
-    private static final String ERROR_GETTING_MAKE_LIST = "Error getting makes list from Vehicle.service";
     VehicleService vehicleService = DefaultVehicleService.getInstance();
 
     @Override
@@ -31,18 +31,26 @@ public class ShowVehicleCreationPageCommand implements Command {
         try {
             List<VehicleMake> makes = vehicleService.getMakeList();
             List<VehicleModel> models = vehicleService.getModelList();
+            List<VehiclePowertrainType> powertrains = vehicleService.getAllPowertrainTypeList();
+            List<VehicleTransmissionType> transmissions = vehicleService.getAllTransmissionTypeList();
+            List<VehicleDriveType> drives = vehicleService.getAllDriveTypeList();
+            List<VehicleColor> colors = vehicleService.getAllColorList();
+            List<VehicleOption> options = vehicleService.getOptionList();
 
-            MakeModelListDto makeModelListDto = new MakeModelListDto();
-            makeModelListDto.setMakes(makes);
-            makeModelListDto.setModels(models);
+            request.setAttribute(VEHICLE_MAKE_LIST, makes);
+            request.setAttribute(VEHICLE_MODEL_LIST,models);
+            request.setAttribute(VEHICLE_POWERTRAIN_LIST, powertrains);
+            request.setAttribute(VEHICLE_TRANSMISSION_LIST, transmissions);
+            request.setAttribute(VEHICLE_DRIVE_LIST, drives);
+            request.setAttribute(VEHICLE_COLOR_LIST, colors);
+            request.setAttribute(VEHICLE_OPTION_LIST, options);
 
-            request.setAttribute(VEHICLE_MAKE_MODEL_LIST, makeModelListDto);
-
-            return new Router(VEHICLE_CREATION_PAGE);
+            return new Router(ADD_VEHICLE_PAGE);
 
         } catch (ServiceException e) {
-            // todo: go to some show_user_error.page
-            logger.error(ERROR_GETTING_MAKE_LIST, e);
+            logger.error("Error getting makes list from Vehicle.service", e);
+            request.setAttribute(EXCEPTION_MESSAGE, e.getMessage());
+
             return new Router(PagePath.ERROR_PAGE);
         }
     }
