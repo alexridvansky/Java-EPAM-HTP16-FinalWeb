ee3553dda4ae8a4727411f3d25c16d8811bf8240
alexridvansky <alexridvansky@gmail.com>
2021-08-13 03:09:21 +0300

    GoToMainPageCommand redirect type changed

--- src/main/java/by/spetr/web/controller/command/redirect/DefaultCommand.java
+++ src/main/java/by/spetr/web/controller/command/redirect/DefaultCommand.java
@@ -2,12 +2,15 @@ package by.spetr.web.controller.command.redirect;
 
 import by.spetr.web.controller.command.Command;
 import by.spetr.web.controller.command.Router;
-import by.spetr.web.controller.command.PagePath;
 import jakarta.servlet.http.HttpServletRequest;
 
+import static by.spetr.web.controller.command.PagePath.ERROR_404_PAGE;
+import static by.spetr.web.controller.command.Router.RouterType.REDIRECT;
+
 public class DefaultCommand implements Command {
+
     @Override
     public Router execute(HttpServletRequest request) {
-        return new Router(PagePath.ERROR_404_PAGE, Router.RouterType.REDIRECT);
+        return new Router(ERROR_404_PAGE, REDIRECT);
     }
 }
