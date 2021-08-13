b5474abdb6060910f754256e6fc379f75544096c
alexridvansky <alexridvansky@gmail.com>
2021-08-13 16:51:21 +0300

    PagePath - personal vehicles page added

--- src/main/java/by/spetr/web/controller/command/PagePath.java
+++ src/main/java/by/spetr/web/controller/command/PagePath.java
@@ -5,11 +5,11 @@ public class PagePath {
     public static final String ADD_VEHICLE_PHOTO_PAGE = "/jsp/add_vehicle_photo.jsp";
     public static final String ADD_PHOTO_PAGE = "/jsp/add_vehicle_photo.jsp";
     public static final String SIGN_UP_PAGE = "/jsp/sign_up.jsp";
-    public static final String SIGN_UP_RESULT_PAGE = "/jsp/sign_up_result.jsp";
-    public static final String SHOW_USER_LIST_ADM = "/jsp/show_user_list_adm.jsp";
-    public static final String SHOW_VEHICLE_LIST = "/jsp/show_vehicle_list.jsp";
-    public static final String SHOW_VEHICLE_LIST_ADM = "/jsp/show_vehicle_list_adm.jsp";
-    public static final String SHOW_VEHICLE_INFO = "/jsp/show_vehicle_info.jsp";
+    public static final String SHOW_USER_LIST_ADM = "/jsp/user_list_adm.jsp";
+    public static final String VEHICLE_LIST_ADM = "/jsp/vehicle_list_adm.jsp";
+    public static final String VEHICLE_LIST_PERSONAL = "/jsp/vehicle_list_personal.jsp";
+    public static final String VEHICLE_LIST_PUBLIC = "/jsp/vehicle_list_public.jsp";
+    public static final String VEHICLE_INFO = "/jsp/vehicle_info.jsp";
     public static final String SHOW_VEHICLE_NOT_FOUND = "/jsp/show_vehicle_not_found.jsp";
     public static final String MAKE_CREATION_PAGE = "/jsp/create_make.jsp";
     public static final String MODEL_CREATION_PAGE = "/jsp/create_model.jsp";
@@ -17,7 +17,6 @@ public class PagePath {
     public static final String ERROR_PAGE = "/jsp/error/error.jsp";
     public static final String INDEX_PAGE = "index.jsp";
     public static final String MAIN_PAGE = "/jsp/main.jsp";
-    public static final String UNSUPPORTED_COMMAND = "/jsp/error/command_error.jsp";
 
     private PagePath() {}
 
