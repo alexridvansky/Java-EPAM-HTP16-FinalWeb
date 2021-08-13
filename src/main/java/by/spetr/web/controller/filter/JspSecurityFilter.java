7fc7de92d2e6ef7b4bd488db65cd896ec771e573
alexridvansky <alexridvansky@gmail.com>
2021-08-13 03:34:08 +0300

    JspSecurityFilter filter added

---
+++ src/main/java/by/spetr/web/controller/filter/JspSecurityFilter.java
@@ -0,0 +1,36 @@
+package by.spetr.web.controller.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebInitParam;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+
+
+@WebFilter(urlPatterns = {"/*"}, initParams = {
+        @WebInitParam(name = "INDEX_PATH", value = "/index.jsp")
+})
+public class JspSecurityFilter implements Filter {
+    private static final Logger logger = LogManager.getLogger();
+    private String indexPath;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        indexPath = filterConfig.getInitParameter("INDEX_PATH");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        String requestURI = httpRequest.getRequestURI();
+        logger.debug("URL: {}", requestURI);
+
+        //httpResponse.sendRedirect(httpRequest.getContextPath() + indexPath);
+        chain.doFilter(request, response);
+    }
+}
