9a0a10088b32d4fc88c48649372bbec88a053037
alexridvansky <alexridvansky@gmail.com>
2021-08-16 11:43:57 +0300

    ChangeVehicleStateCommand - error handling and logging improved

--- src/main/java/by/spetr/web/controller/command/vehicle/ChangeVehicleStateCommand.java
+++ src/main/java/by/spetr/web/controller/command/vehicle/ChangeVehicleStateCommand.java
@@ -3,8 +3,12 @@ package by.spetr.web.controller.command.vehicle;
 import by.spetr.web.controller.command.Command;
 import by.spetr.web.controller.command.PagePath;
 import by.spetr.web.controller.command.Router;
+import by.spetr.web.model.dto.UserDto;
 import by.spetr.web.model.entity.type.VehicleStateType;
 import by.spetr.web.model.exception.ServiceException;
+import by.spetr.web.model.form.DefaultForm;
+import by.spetr.web.model.form.VehicleFullForm;
+import by.spetr.web.model.form.VehicleShortForm;
 import by.spetr.web.model.service.DefaultVehicleService;
 import by.spetr.web.model.service.VehicleService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -25,22 +29,46 @@ public class ChangeVehicleStateCommand implements Command {
     public Router execute(HttpServletRequest request) {
         logger.info("ChangeVehicleStateCommand() method been called");
 
-        String vehicleId = request.getParameter(VEHICLE_ID_PARAM);
-        String vehicleState = request.getParameter(VEHICLE_STATE_PARAM);
+        VehicleFullForm form = (VehicleFullForm) doForm(request);
 
         try {
-            vehicleService.updateVehicleState(Long.parseLong(vehicleId), VehicleStateType.valueOf(vehicleState));
-            logger.debug("vehicleId '{}' - '{}'", vehicleId, vehicleState);
+            vehicleService.updateVehicleState(form);
+            logger.debug("vehicleId '{}' - '{}'", form.getVehicleId(), form.getState());
 
             String lastPage = (String) request.getSession().getAttribute(LAST_PAGE_PARAM);
 
             return new Router(Objects.requireNonNullElse(lastPage, PagePath.INDEX_PAGE));
 
         } catch (ServiceException e) {
-            logger.error("state for the 'vehicleId {}' not changed", vehicleId);
+            logger.error("state for the 'vehicleId {}' not changed", form.getVehicleId());
             request.setAttribute(EXCEPTION_MESSAGE, "state not changed");
 
             return new Router(ERROR_PAGE, REDIRECT);
+        } catch (IllegalArgumentException e) {
+            logger.error("Wrong parameters' types, parsing error", e);
+            request.setAttribute(EXCEPTION_MESSAGE, "Wrong parameters' types, parsing error");
+
+            return new Router(ERROR_PAGE);
         }
+    }
+
+    @Override
+    public DefaultForm doForm(HttpServletRequest request) {
+        VehicleFullForm form = new VehicleFullForm();
+
+        UserDto executor = (UserDto) request.getSession().getAttribute(USER_PARAM);
+        String vehicleId = request.getParameter(VEHICLE_ID_PARAM);
+        String vehicleState = request.getParameter(VEHICLE_STATE_PARAM);
+
+        if (vehicleId == null || vehicleState == null || executor == null) {
+            logger.error("Wrong parameters' types, parsing error");
+            throw new IllegalArgumentException("Wrong parameters' types, parsing error");
+        }
+
+        form.setExecutor(executor);
+        form.setVehicleId(Long.parseLong(vehicleId));
+        form.setState(VehicleStateType.valueOf(vehicleState));
+
+        return form;
     }
 }
