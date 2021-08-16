1ea2b437b229ad35dcde64c91d2906d75979f16d
alexridvansky <alexridvansky@gmail.com>
2021-08-16 12:11:49 +0300

    ShowVehicleListPersonalCommand - command refurbished

--- src/main/java/by/spetr/web/controller/command/vehicle/ShowVehicleCreationPageCommand.java
+++ src/main/java/by/spetr/web/controller/command/vehicle/ShowVehicleCreationPageCommand.java
@@ -3,11 +3,18 @@ package by.spetr.web.controller.command.vehicle;
 import by.spetr.web.controller.command.Command;
 import by.spetr.web.controller.command.PagePath;
 import by.spetr.web.controller.command.Router;
-import by.spetr.web.model.entity.*;
+import by.spetr.web.model.dto.UserDto;
+import by.spetr.web.model.entity.VehicleColor;
+import by.spetr.web.model.entity.VehicleMake;
+import by.spetr.web.model.entity.VehicleModel;
+import by.spetr.web.model.entity.VehicleOption;
 import by.spetr.web.model.entity.type.VehicleDriveType;
 import by.spetr.web.model.entity.type.VehiclePowertrainType;
 import by.spetr.web.model.entity.type.VehicleTransmissionType;
 import by.spetr.web.model.exception.ServiceException;
+import by.spetr.web.model.form.DefaultForm;
+import by.spetr.web.model.form.VehicleFullForm;
+import by.spetr.web.model.service.AccessControlService;
 import by.spetr.web.model.service.DefaultVehicleService;
 import by.spetr.web.model.service.VehicleService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -17,18 +24,25 @@ import org.apache.logging.log4j.Logger;
 import java.util.List;
 
 import static by.spetr.web.controller.command.PagePath.ADD_VEHICLE_PAGE;
+import static by.spetr.web.controller.command.PagePath.ERROR_PAGE;
 import static by.spetr.web.controller.command.RequestParameter.*;
+import static by.spetr.web.controller.command.Router.RouterType.REDIRECT;
 
 
 public class ShowVehicleCreationPageCommand implements Command {
     private static final Logger logger = LogManager.getLogger();
     VehicleService vehicleService = DefaultVehicleService.getInstance();
+    AccessControlService accessControlService = AccessControlService.getInstance();
 
     @Override
     public Router execute(HttpServletRequest request) {
         logger.debug("ShowVehicleCreationPageCommand() called");
 
         try {
+            VehicleFullForm form = (VehicleFullForm) doForm(request);
+
+            accessControlService.createVehicle(form);
+
             List<VehicleMake> makes = vehicleService.getMakeList();
             List<VehicleModel> models = vehicleService.getModelList();
             List<VehiclePowertrainType> powertrains = vehicleService.getAllPowertrainTypeList();
@@ -38,7 +52,7 @@ public class ShowVehicleCreationPageCommand implements Command {
             List<VehicleOption> options = vehicleService.getOptionList();
 
             request.setAttribute(VEHICLE_MAKE_LIST, makes);
-            request.setAttribute(VEHICLE_MODEL_LIST,models);
+            request.setAttribute(VEHICLE_MODEL_LIST, models);
             request.setAttribute(VEHICLE_POWERTRAIN_LIST, powertrains);
             request.setAttribute(VEHICLE_TRANSMISSION_LIST, transmissions);
             request.setAttribute(VEHICLE_DRIVE_LIST, drives);
@@ -51,7 +65,26 @@ public class ShowVehicleCreationPageCommand implements Command {
             logger.error("Error getting makes list from Vehicle.service", e);
             request.setAttribute(EXCEPTION_MESSAGE, e.getMessage());
 
-            return new Router(PagePath.ERROR_PAGE);
+            return new Router(ERROR_PAGE, REDIRECT);
+
+        } catch (IllegalArgumentException e) {
+            logger.error(e.getMessage(), e);
+            request.setAttribute(EXCEPTION_MESSAGE, e.getMessage());
+
+            return new Router(ERROR_PAGE, REDIRECT);
         }
+    }
+
+    @Override
+    public DefaultForm doForm(HttpServletRequest request) {
+        VehicleFullForm form = new VehicleFullForm();
+        UserDto executor = (UserDto) request.getSession().getAttribute(USER_PARAM);
+        if (executor == null) {
+            logger.error("No user in sessionScope");
+            throw new IllegalArgumentException("No user in sessionScope");
+        }
+        form.setExecutor(executor);
+
+        return form;
     }
 }
