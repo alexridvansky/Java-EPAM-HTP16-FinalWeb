5dbde8feca566aa2247e7a331215143390c0fb49
alexridvansky <alexridvansky@gmail.com>
2021-08-16 20:35:21 +0300

    ShowModelCreationPageCommand - messaging improved

--- src/main/java/by/spetr/web/controller/command/vehicle/ShowModelCreationPageCommand.java
+++ src/main/java/by/spetr/web/controller/command/vehicle/ShowModelCreationPageCommand.java
@@ -2,10 +2,8 @@ package by.spetr.web.controller.command.vehicle;
 
 import by.spetr.web.controller.command.Command;
 import by.spetr.web.controller.command.Router;
-import by.spetr.web.model.dto.UserDto;
 import by.spetr.web.model.entity.VehicleMake;
 import by.spetr.web.model.exception.ServiceException;
-import by.spetr.web.model.form.DefaultForm;
 import by.spetr.web.model.service.DefaultVehicleService;
 import by.spetr.web.model.service.VehicleService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -18,8 +16,6 @@ import static by.spetr.web.controller.command.PagePath.ERROR_PAGE;
 import static by.spetr.web.controller.command.PagePath.MODEL_CREATION_PAGE;
 import static by.spetr.web.controller.command.RequestParameter.*;
 import static by.spetr.web.controller.command.Router.RouterType.REDIRECT;
-import static by.spetr.web.model.entity.type.UserRoleType.MODERATOR;
-import static by.spetr.web.model.entity.type.UserRoleType.ROOT;
 
 
 public class ShowModelCreationPageCommand implements Command {
@@ -39,7 +35,7 @@ public class ShowModelCreationPageCommand implements Command {
 
         } catch (ServiceException e) {
             logger.error("Error getting makes list from Vehicle.service", e);
-            request.setAttribute(FEEDBACK_MESSAGE, "Error getting makes list from Vehicle.service");
+            request.setAttribute(FEEDBACK_MESSAGE_PARAM, "Error getting makes list from Vehicle.service");
             request.setAttribute(EXCEPTION_MESSAGE, e.getMessage());
 
             return new Router(ERROR_PAGE, REDIRECT);
