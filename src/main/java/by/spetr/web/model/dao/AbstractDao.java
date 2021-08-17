c3269e564ecf7f3e18661ebbfe447afde04465af
alexridvansky <alexridvansky@gmail.com>
2021-08-17 04:29:50 +0300

    AbstractDao - methods added

--- src/main/java/by/spetr/web/model/dao/AbstractDao.java
+++ src/main/java/by/spetr/web/model/dao/AbstractDao.java
@@ -1,8 +1,13 @@
 package by.spetr.web.model.dao;
 
 import by.spetr.web.model.entity.AbstractEntity;
+import by.spetr.web.model.exception.DaoException;
+
+import java.util.List;
+import java.util.Optional;
 
 
 public abstract class AbstractDao <T extends AbstractEntity> {
-
+    abstract List<T> findAll() throws DaoException;
+    abstract Optional<T> findById(long id) throws DaoException;
 }
