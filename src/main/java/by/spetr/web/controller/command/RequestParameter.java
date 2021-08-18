8d00051e74b694082fdd20449ae98d2b8a0467fc
alexridvansky <alexridvansky@gmail.com>
2021-08-18 21:53:14 +0300

    RequestParameter - new parameters added

--- src/main/java/by/spetr/web/controller/command/RequestParameter.java
+++ src/main/java/by/spetr/web/controller/command/RequestParameter.java
@@ -3,22 +3,32 @@ package by.spetr.web.controller.command;
 public final class RequestParameter {
     private RequestParameter() {}
 
-    public static final String COMMAND = "command";
+    // User params
     public static final String USER_LIST_PARAM = "users";
     public static final String USER_PARAM = "user";
     public static final String USER_NAME_PARAM = "username";
+    public static final String REG_USER_NAME_PARAM = "reg_username";
     public static final String USER_STATE_PARAM = "new_state";
     public static final String USER_ROLE_PARAM = "new_role";
+    public static final String USER_PASSWORD_PARAM = "password";
+    public static final String USER_PASSWORD_AGAIN_PARAM = "password_again";
+    public static final String USER_EMAIL_PARAM = "email";
+    public static final String USER_PHONE_PARAM = "phone";
+    public static final String USER_REGISTRATION_DATA_PARAM = "reg_data";
+    
+    // Vehicle params
     public static final String VEHICLE_LIST_PARAM = "vehicles";
     public static final String VEHICLE_PARAM = "vehicle";
     public static final String VEHICLE_ID_PARAM = "vehicle_id";
     public static final String VEHICLE_STATE_PARAM = "vehicle_state";
     public static final String VEHICLE_MAKE_ID_PARAM = "make_id";
     public static final String VEHICLE_MODEL_ID_PARAM = "model_id";
+    public static final String VEHICLE_LAST_MAKE_ID = "last_make_id";
     public static final String VEHICLE_POWERTRAIN_ID_PARAM = "powertrain_id";
     public static final String VEHICLE_TRANSMISSION_ID_PARAM = "transmission_id";
     public static final String VEHICLE_DRIVE_ID_PARAM = "drive_id";
     public static final String VEHICLE_COLOR_ID_PARAM = "color_id";
+    public static final String VEHICLE_COLOR_PARAM = "color";
     public static final String VEHICLE_MAKE_PARAM = "make";
     public static final String VEHICLE_MODEL_PARAM = "model";
     public static final String VEHICLE_MODEL_YEAR_PARAM = "model_year";
@@ -27,27 +37,27 @@ public final class RequestParameter {
     public static final String VEHICLE_POWER_PARAM = "power";
     public static final String VEHICLE_DESCRIPTION_PARAM = "description";
     public static final String VEHICLE_PRICE_PARAM = "price";
-    public static final String VEHICLE_MAKE_LIST = "makes";
-    public static final String VEHICLE_MODEL_LIST = "models";
-    public static final String VEHICLE_OPTION_LIST = "options";
-    public static final String VEHICLE_OPTIONS_SET = "options_set";
-    public static final String VEHICLE_POWERTRAIN_LIST = "powertrains";
-    public static final String VEHICLE_TRANSMISSION_LIST = "transmissions";
-    public static final String VEHICLE_DRIVE_LIST = "drives";
-    public static final String VEHICLE_COLOR_LIST = "colors";
-    public static final String VEHICLE_MAKE_MODEL_LIST = "makes_models";
-    public static final String PASSWORD_PARAM = "password";
-    public static final String PASSWORD_AGAIN_PARAM = "password_again";
-    public static final String EMAIL_PARAM = "email";
-    public static final String PHONE_PARAM = "phone";
-    public static final String REGISTRATION_DATA = "reg_data";
-    public static final String AUTHENTICATION_ERROR = "auth_error";
+    public static final String VEHICLE_MAKE_LIST_PARAM = "makes";
+    public static final String VEHICLE_MODEL_LIST_PARAM = "models";
+    public static final String VEHICLE_OPTION_LIST_PARAM = "options";
+    public static final String VEHICLE_OPTIONS_SET_PARAM = "options_set";
+    public static final String VEHICLE_POWERTRAIN_LIST_PARAM = "powertrains";
+    public static final String VEHICLE_TRANSMISSION_LIST_PARAM = "transmissions";
+    public static final String VEHICLE_DRIVE_LIST_PARAM = "drives";
+    public static final String VEHICLE_COLOR_LIST_PARAM = "colors";
+
+    // Navigation and informing params
+    public static final String COMMAND_PARAM = "command";
+    public static final String AUTHENTICATION_ERROR_PARAM = "auth_error";
     public static final String LAST_PAGE_PARAM = "last_page";
     public static final String LOCALE_PARAM = "locale";
     public static final String NEW_LOCALE_PARAM = "new_locale";
     public static final String FILENAME_PARAM = "filename";
-    public static final String FEEDBACK_MESSAGE = "feedback_msg";
-    public static final String EXCEPTION_MESSAGE = "e";
+    public static final String FEEDBACK_MESSAGE_PARAM = "feedback_msg";
+    public static final String OPERATION_SUCCESS_PARAM = "success";
+    public static final String EXCEPTION_MESSAGE_PARAM = "e";
     public static final String MESSAGE_PARAM = "message";
     public static final String PREVIEW_PARAM = "preview";
+    public static final String PAGE_PARAM = "page";
+    public static final String PAGEABLE = "pageable";
 }
