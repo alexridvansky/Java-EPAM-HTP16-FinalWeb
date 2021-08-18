746cc6f08854692b5c9440bdd72df282fd81e735
alexridvansky <alexridvansky@gmail.com>
2021-08-18 22:21:29 +0300

    ShowVehicleListPublicCommand - pagination support added

--- src/main/java/by/spetr/web/controller/command/vehicle/ShowVehicleListPublicCommand.java
+++ src/main/java/by/spetr/web/controller/command/vehicle/ShowVehicleListPublicCommand.java
@@ -1,33 +1,48 @@
 package by.spetr.web.controller.command.vehicle;
 
 import by.spetr.web.controller.command.Command;
+import by.spetr.web.controller.command.Page;
 import by.spetr.web.controller.command.Router;
 import by.spetr.web.model.dto.VehiclePreviewDto;
 import by.spetr.web.model.exception.ServiceException;
 import by.spetr.web.model.service.DefaultVehicleService;
 import by.spetr.web.model.service.VehicleService;
+import by.spetr.web.util.PropertyUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static by.spetr.web.controller.command.PagePath.*;
-import static by.spetr.web.controller.command.RequestParameter.VEHICLE_LIST_PARAM;
-import static by.spetr.web.controller.command.Router.RouterType.REDIRECT;
+import static by.spetr.web.controller.command.PagePath.ERROR_PAGE;
+import static by.spetr.web.controller.command.PagePath.VEHICLE_LIST_PUBLIC;
+import static by.spetr.web.controller.command.RequestParameter.*;
 
 public class ShowVehicleListPublicCommand implements Command {
+    private static final Logger logger = LogManager.getLogger();
     private static final VehicleService vehicleService = DefaultVehicleService.getInstance();
 
     @Override
     public Router execute(HttpServletRequest request) {
         try {
-            List<VehiclePreviewDto> vehicles = vehicleService.getPublicVehicleList();
+            int adCount = vehicleService.getPublicVehicleListSize();
+            List<VehiclePreviewDto> vehicles = new ArrayList<>(adCount);
+            int pageToDisplay = getPage(request);
+            if (adCount > 0) {
+                vehicles = vehicleService.getPublicVehicleList(Page.PAGE_SIZE, pageToDisplay);
+            }
 
+            request.setAttribute(PAGEABLE, new Page(adCount, pageToDisplay, Page.PAGE_SIZE));
             request.setAttribute(VEHICLE_LIST_PARAM, vehicles);
 
             return new Router(VEHICLE_LIST_PUBLIC);
 
         } catch (ServiceException e) {
-            return new Router(ERROR_PAGE, REDIRECT);
+            logger.error(e);
+            request.setAttribute(EXCEPTION_MESSAGE_PARAM, e.getMessage());
+
+            return new Router(ERROR_PAGE);
         }
     }
 }
