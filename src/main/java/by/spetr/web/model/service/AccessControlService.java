0bea8aa111c86be884b6851f7a49c0973e77728d
alexridvansky <alexridvansky@gmail.com>
2021-08-18 23:30:08 +0300

    AccessControlService - interface added

---
+++ src/main/java/by/spetr/web/model/service/AccessControlService.java
@@ -0,0 +1,31 @@
+package by.spetr.web.model.service;
+
+import by.spetr.web.controller.command.CommandType;
+import by.spetr.web.model.entity.type.UserRoleType;
+import by.spetr.web.model.exception.ServiceException;
+import by.spetr.web.model.form.UserForm;
+import by.spetr.web.model.form.VehicleFullForm;
+
+public interface AccessControlService {
+
+    /**
+     * is used to check users' authorisation to access within commands
+     *
+     * @param role    UserRoleType stands for User.role
+     * @param command CommandType stands for Command name
+     * @return true if operation is allowed for user group given
+     */
+    boolean commandPermission(UserRoleType role, CommandType command);
+
+    boolean updateUserRole(UserForm form) throws ServiceException;
+
+    boolean updateUserState(UserForm form) throws ServiceException;
+
+    boolean updateVehicleState(VehicleFullForm form) throws ServiceException;
+
+    boolean createVehicle(VehicleFullForm form) throws ServiceException;
+
+    static DefaultAccessControlService getInstance() {
+        return DefaultAccessControlService.getInstance();
+    }
+}
