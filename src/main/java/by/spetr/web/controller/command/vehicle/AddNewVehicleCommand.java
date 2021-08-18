9f6cbbdd6eee03eb0c0cde267fc18eb709c4024c
alexridvansky <alexridvansky@gmail.com>
2021-08-18 22:42:03 +0300

    AddNewVehicleCommand - command refurbished

--- src/main/java/by/spetr/web/controller/command/vehicle/AddNewVehicleCommand.java
+++ src/main/java/by/spetr/web/controller/command/vehicle/AddNewVehicleCommand.java
@@ -4,7 +4,6 @@ import by.spetr.web.controller.command.Command;
 import by.spetr.web.controller.command.Router;
 import by.spetr.web.model.dto.UserDto;
 import by.spetr.web.model.entity.Vehicle;
-import by.spetr.web.model.entity.type.UserRoleType;
 import by.spetr.web.model.exception.ServiceException;
 import by.spetr.web.model.form.DefaultForm;
 import by.spetr.web.model.form.VehicleFullForm;
@@ -19,7 +18,6 @@ import java.util.Set;
 import static by.spetr.web.controller.command.PagePath.ERROR_PAGE;
 import static by.spetr.web.controller.command.PagePath.VEHICLE_LIST_PERSONAL;
 import static by.spetr.web.controller.command.RequestParameter.*;
-import static by.spetr.web.controller.command.Router.RouterType.REDIRECT;
 
 public class AddNewVehicleCommand implements Command {
     private static final Logger logger = LogManager.getLogger();
@@ -30,25 +28,20 @@ public class AddNewVehicleCommand implements Command {
         logger.debug("Create new vehicle command");
 
         UserDto user = (UserDto) request.getSession().getAttribute(USER_PARAM);
-        if (user.getRole() != UserRoleType.USER) {
-            logger.error("Unauthorised access attempt");
-            request.setAttribute(EXCEPTION_MESSAGE, "Unauthorised access attempt");
-
-            return new Router(ERROR_PAGE, REDIRECT);
-        }
-
         try {
             VehicleFullForm form = (VehicleFullForm) doForm(request);
             Vehicle vehicle = vehicleService.addVehicle(form);
             request.setAttribute(VEHICLE_PARAM, vehicle);
+            request.setAttribute(FEEDBACK_MESSAGE_PARAM, form.getFeedbackMsg());
+            request.setAttribute(OPERATION_SUCCESS_PARAM, form.isSuccess());
 
-            return new Router(VEHICLE_LIST_PERSONAL, REDIRECT);
+            return new Router(VEHICLE_LIST_PERSONAL);
 
         } catch (IllegalArgumentException | ServiceException e) {
             logger.error(e);
-            request.setAttribute(EXCEPTION_MESSAGE, e.getMessage());
+            request.setAttribute(EXCEPTION_MESSAGE_PARAM, e.getMessage());
 
-            return new Router(ERROR_PAGE, REDIRECT);
+            return new Router(ERROR_PAGE);
         }
 
     }
@@ -59,28 +52,29 @@ public class AddNewVehicleCommand implements Command {
             VehicleFullForm form = new VehicleFullForm();
 
             UserDto user = (UserDto) request.getSession().getAttribute(USER_PARAM);
-            form.setOwnerId(user.getUserId());
+            assert user != null : "No user in sessionScope";
             int makeId = Integer.parseInt(request.getParameter(VEHICLE_MAKE_ID_PARAM));
-            form.setMakeId(makeId);
             int modelId = Integer.parseInt(request.getParameter(VEHICLE_MODEL_ID_PARAM));
-            form.setModelId(modelId);
             int powertrain = Integer.parseInt(request.getParameter(VEHICLE_POWERTRAIN_ID_PARAM));
-            form.setPowertrainId(powertrain);
             int transmissionId = Integer.parseInt(request.getParameter(VEHICLE_TRANSMISSION_ID_PARAM));
-            form.setTransmissionId(transmissionId);
             int driveId = Integer.parseInt(request.getParameter(VEHICLE_DRIVE_ID_PARAM));
-            form.setDriveId(driveId);
             int colorId = Integer.parseInt(request.getParameter(VEHICLE_COLOR_ID_PARAM));
-            form.setColorId(colorId);
             int modelYear = Integer.parseInt(request.getParameter(VEHICLE_MODEL_YEAR_PARAM));
-            form.setModelYear(Year.of(modelYear));
             int displacement = Integer.parseInt(request.getParameter(VEHICLE_DISPLACEMENT_PARAM));
-            form.setDisplacement(displacement);
             int mileage = Integer.parseInt(request.getParameter(VEHICLE_MILEAGE_PARAM));
-            form.setMileage(mileage);
             int power = Integer.parseInt(request.getParameter(VEHICLE_POWER_PARAM));
-            form.setPower(power);
             int price = Integer.parseInt(request.getParameter(VEHICLE_PRICE_PARAM));
+            form.setOwnerId(user.getUserId());
+            form.setMakeId(makeId);
+            form.setModelId(modelId);
+            form.setPowertrainId(powertrain);
+            form.setTransmissionId(transmissionId);
+            form.setDriveId(driveId);
+            form.setColorId(colorId);
+            form.setModelYear(Year.of(modelYear));
+            form.setDisplacement(displacement);
+            form.setMileage(mileage);
+            form.setPower(power);
             form.setPrice(price);
 
             String description = request.getParameter(VEHICLE_DESCRIPTION_PARAM);
@@ -89,7 +83,7 @@ public class AddNewVehicleCommand implements Command {
             }
             form.setComment(description);
 
-            String[] optionSet = request.getParameterValues(VEHICLE_OPTIONS_SET);
+            String[] optionSet = request.getParameterValues(VEHICLE_OPTIONS_SET_PARAM);
             if (optionSet != null) {
                 for (String option : optionSet) {
                     long optionId = Long.parseLong(option);
@@ -103,9 +97,9 @@ public class AddNewVehicleCommand implements Command {
 
             return form;
 
-        } catch (NumberFormatException | NullPointerException e) {
-            logger.error("Weird thing... Wrong parameters' types, parsing error");
-            throw new IllegalArgumentException("Weird thing... Wrong parameters' types, parsing error");
+        } catch (NumberFormatException | NullPointerException | AssertionError e) {
+            logger.error("Wrong parameters' types, parsing error");
+            throw new IllegalArgumentException("Wrong parameters' types, parsing error");
         }
     }
 }
