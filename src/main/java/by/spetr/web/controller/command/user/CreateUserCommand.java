fadf2587c6b46be6546a019a834845910f711822
alexridvansky <alexridvansky@gmail.com>
2021-08-18 21:59:25 +0300

    CreateUserCommand - command refurbished

--- src/main/java/by/spetr/web/controller/command/user/CreateUserCommand.java
+++ src/main/java/by/spetr/web/controller/command/user/CreateUserCommand.java
@@ -1,11 +1,11 @@
 package by.spetr.web.controller.command.user;
 
 import by.spetr.web.controller.command.Command;
-import by.spetr.web.controller.command.PagePath;
 import by.spetr.web.controller.command.Router;
 import by.spetr.web.model.dto.UserDto;
-import by.spetr.web.model.form.UserRegistrationForm;
 import by.spetr.web.model.exception.ServiceException;
+import by.spetr.web.model.form.DefaultForm;
+import by.spetr.web.model.form.UserRegForm;
 import by.spetr.web.model.service.DefaultUserService;
 import by.spetr.web.model.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,7 +15,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.Objects;
 import java.util.Optional;
 
-import static by.spetr.web.controller.command.PagePath.ERROR_PAGE;
+import static by.spetr.web.controller.command.PagePath.*;
 import static by.spetr.web.controller.command.RequestParameter.*;
 
 public class CreateUserCommand implements Command {
@@ -27,30 +27,37 @@ public class CreateUserCommand implements Command {
         logger.debug("CreateUserCommand called");
         String lastPage = (String) request.getSession().getAttribute(LAST_PAGE_PARAM);
 
-        UserRegistrationForm userForm = new UserRegistrationForm(
-                request.getParameter(USER_NAME_PARAM),
-                request.getParameter(PASSWORD_PARAM),
-                request.getParameter(PASSWORD_AGAIN_PARAM),
-                request.getParameter(EMAIL_PARAM),
-                request.getParameter(PHONE_PARAM)
-        );
+        UserRegForm form = (UserRegForm) doForm(request);
 
         try {
-            Optional<UserDto> optionalUserDto = userService.registerUser(userForm);
+            Optional<UserDto> optionalUserDto = userService.registerUser(form);
             if (optionalUserDto.isEmpty()) {
-                request.setAttribute(REGISTRATION_DATA, userForm);
+                request.setAttribute(USER_REGISTRATION_DATA_PARAM, form);
+                request.setAttribute(FEEDBACK_MESSAGE_PARAM, form.getFeedbackMsg());
 
-                return new Router(PagePath.SIGN_UP_PAGE);
+                return new Router(SIGN_UP_PAGE);
             } else {
                 request.getSession().setAttribute(USER_PARAM, optionalUserDto.get());
 
-                return new Router(Objects.requireNonNullElse(lastPage, PagePath.INDEX_PAGE));
+                return new Router(Objects.requireNonNullElse(lastPage, INDEX_PAGE));
             }
         } catch (ServiceException e) {
             logger.error(e);
-            request.setAttribute(EXCEPTION_MESSAGE, e.getMessage());
+            request.setAttribute(EXCEPTION_MESSAGE_PARAM, e.getMessage());
 
             return new Router(ERROR_PAGE);
         }
+    }
+
+    @Override
+    public DefaultForm doForm(HttpServletRequest request) {
+        UserRegForm form = new UserRegForm();
+        form.setLogin(request.getParameter(REG_USER_NAME_PARAM));
+        form.setPassword(request.getParameter(USER_PASSWORD_PARAM));
+        form.setPasswordRepeat(request.getParameter(USER_PASSWORD_AGAIN_PARAM));
+        form.setEmail(request.getParameter(USER_EMAIL_PARAM));
+        form.setPhone(request.getParameter(USER_PHONE_PARAM));
+
+        return form;
     }
 }
