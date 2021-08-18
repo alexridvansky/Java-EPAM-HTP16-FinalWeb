071a6c6661b4939d46a3dd188582bfdecfe0c31d
alexridvansky <alexridvansky@gmail.com>
2021-08-18 21:32:00 +0300

    UserRegForm - class added

---
+++ src/main/java/by/spetr/web/model/form/UserRegForm.java
@@ -0,0 +1,49 @@
+package by.spetr.web.model.form;
+
+public class UserRegForm extends DefaultForm {
+    private String login;
+    private String password;
+    private String passwordRepeat;
+    private String email;
+    private String phone;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordRepeat() {
+        return passwordRepeat;
+    }
+
+    public void setPasswordRepeat(String passwordRepeat) {
+        this.passwordRepeat = passwordRepeat;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
