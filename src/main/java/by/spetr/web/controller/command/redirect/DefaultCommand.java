f6c2bff27631e25aab52d114de578d34b18ecbb2
alexridvansky <alexridvansky@gmail.com>
2021-08-18 17:49:49 +0300

    DefaultCommand - redirect source changed to main.page

--- src/main/java/by/spetr/web/controller/command/redirect/DefaultCommand.java
+++ src/main/java/by/spetr/web/controller/command/redirect/DefaultCommand.java
@@ -4,13 +4,13 @@ import by.spetr.web.controller.command.Command;
 import by.spetr.web.controller.command.Router;
 import jakarta.servlet.http.HttpServletRequest;
 
-import static by.spetr.web.controller.command.PagePath.ERROR_404_PAGE;
+import static by.spetr.web.controller.command.PagePath.MAIN_PAGE;
 import static by.spetr.web.controller.command.Router.RouterType.REDIRECT;
 
 public class DefaultCommand implements Command {
 
     @Override
     public Router execute(HttpServletRequest request) {
-        return new Router(ERROR_404_PAGE, REDIRECT);
+        return new Router(MAIN_PAGE, REDIRECT);
     }
 }
