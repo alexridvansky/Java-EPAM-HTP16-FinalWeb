0c159d23979aa5f2519272427716dfd2823c69eb
alexridvansky <alexridvansky@gmail.com>
2021-08-23 14:54:06 +0300

    UserValidator, VehicleValidator - utility supply method renamed

--- src/main/java/by/spetr/web/validator/UserValidator.java
+++ src/main/java/by/spetr/web/validator/UserValidator.java
@@ -1,6 +1,6 @@
 package by.spetr.web.validator;
 
-import by.spetr.web.util.PropertyUtil;
+import by.spetr.web.util.PropertyReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -9,12 +9,12 @@ import java.util.regex.Pattern;
 
 public class UserValidator {
     private static final Logger logger = LogManager.getLogger();
-    private static final String REGEXP_USERNAME = "regexp.username";
-    private static final String REGEXP_PASSWORD = "regexp.password";
-    private static final String REGEXP_EMAIL = "regexp.email";
-    private static final String REGEXP_PHONE = "regexp.phone";
+    private static final String REGEXP_USERNAME = "regexp.user.username";
+    private static final String REGEXP_PASSWORD = "regexp.user.password";
+    private static final String REGEXP_EMAIL = "regexp.user.email";
+    private static final String REGEXP_PHONE = "regexp.user.phone";
 
-    private static final PropertyUtil PROPERTY_UTIL = PropertyUtil.getInstance();
+    private static final PropertyReader PROPERTY_UTIL = PropertyReader.getInstance();
 
     private UserValidator() {}
 
