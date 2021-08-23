39eca0e37a2dc238b918592c8d9a11c8cee0e4f3
alexridvansky <alexridvansky@gmail.com>
2021-08-23 14:52:41 +0300

    PasswordGenerator, PasswordGeneratorTest - classes added

---
+++ src/main/java/by/spetr/web/util/PasswordGenerator.java
@@ -0,0 +1,28 @@
+package by.spetr.web.util;
+
+import java.security.SecureRandom;
+
+public class PasswordGenerator {
+    private static final SecureRandom random = new SecureRandom();
+    private static final String PASS_CHARSET_PROPERTY = "site.generate_pass_charset";
+    private static final String PASS_LENGTH_PROPERTY = "site.generate_pass_length";
+    private static final String CHAR_SET;
+    private static final int PASS_LENGTH;
+
+    static {
+        CHAR_SET = PropertyReader.getInstance().getSiteProperty(PASS_CHARSET_PROPERTY);
+        PASS_LENGTH = Integer.parseInt(PropertyReader.getInstance().getSiteProperty(PASS_LENGTH_PROPERTY));
+    }
+
+    private PasswordGenerator() {}
+
+    public static String generate() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < PASS_LENGTH; i++) {
+            int randomIndex = random.nextInt(CHAR_SET.length());
+            sb.append(CHAR_SET.charAt(randomIndex));
+        }
+
+        return sb.toString();
+    }
+}
