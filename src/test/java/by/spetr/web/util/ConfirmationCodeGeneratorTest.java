d0f386ececcb2754565cadd569c2e10a9ef1c8a6
alexridvansky <alexridvansky@gmail.com>
2021-08-23 09:07:19 +0300

    test

--- src/test/java/by/spetr/web/util/ConfirmationCodeGeneratorTest.java
+++ src/test/java/by/spetr/web/util/ConfirmationCodeGeneratorTest.java
@@ -4,14 +4,27 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ConfirmationCodeGeneratorTest {
+    private static final int EXPECTED_CODE_LENGTH = 6;
+
+    @Test
+    public void confirmCodeNotNullTest() {
+        String code = ConfirmationCodeGenerator.generateConfirmCode();
+
+        Assert.assertNotNull(code);
+    }
 
     @Test
     public void confirmCodeLengthTest() {
-        Assert.assertEquals(ConfirmationCodeGenerator.generateConfirmCode().length(), 6);
+        String code = ConfirmationCodeGenerator.generateConfirmCode();
+
+        Assert.assertEquals(code.length(), EXPECTED_CODE_LENGTH);
     }
 
     @Test
     public void confirmCodeNotEqualityTest() {
-        Assert.assertFalse(ConfirmationCodeGenerator.generateConfirmCode().equals(ConfirmationCodeGenerator.generateConfirmCode()));
+        String code1 = ConfirmationCodeGenerator.generateConfirmCode();
+        String code2 = ConfirmationCodeGenerator.generateConfirmCode();
+
+        Assert.assertNotEquals(code1, code2);
     }
 }
