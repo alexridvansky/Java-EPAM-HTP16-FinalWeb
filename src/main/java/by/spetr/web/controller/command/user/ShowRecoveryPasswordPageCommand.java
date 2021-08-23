65ad06361f99b6ecb89fda741f37217cc77cb6b0
alexridvansky <alexridvansky@gmail.com>
2021-08-23 14:20:29 +0300

    ShowRecoveryPasswordPageCommand - command added

---
+++ src/main/java/by/spetr/web/controller/command/user/ShowRecoveryPasswordPageCommand.java
@@ -0,0 +1,20 @@
+package by.spetr.web.controller.command.user;
+
+import by.spetr.web.controller.command.Command;
+import by.spetr.web.controller.command.Router;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import static by.spetr.web.controller.command.PagePath.PASSWORD_RECOVERY_PAGE;
+
+public class ShowRecoveryPasswordPageCommand implements Command {
+    private static final Logger logger = LogManager.getLogger();
+
+    @Override
+    public Router execute(HttpServletRequest request) {
+        logger.debug("redirecting to password recovery page");
+
+        return new Router(PASSWORD_RECOVERY_PAGE);
+    }
+}
