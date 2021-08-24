ae969b985a86c1bb06d31453b10a6681999f1465
alexridvansky <alexridvansky@gmail.com>
2021-08-24 12:53:53 +0300

    InformerService - class added

---
+++ src/main/java/by/spetr/web/telegrambot/InformerService.java
@@ -0,0 +1,15 @@
+package by.spetr.web.telegrambot;
+
+import org.telegram.telegrambots.meta.api.objects.Message;
+
+public interface InformerService {
+    void sendMessage(String chatId, String text);
+
+    void sendPublicMessage(String text);
+
+    void sendMessage(Message message, String text);
+
+    static InformerService getInstance() {
+        return new TelegramInformerService();
+    }
+}
