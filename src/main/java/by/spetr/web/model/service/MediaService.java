27cf710772567fbf12d856d35dced9d3ca1457c2
alexridvansky <alexridvansky@gmail.com>
2021-08-24 15:05:15 +0300

    MediaService - class added

---
+++ src/main/java/by/spetr/web/model/service/MediaService.java
@@ -0,0 +1,11 @@
+package by.spetr.web.model.service;
+
+import by.spetr.web.model.exception.ServiceException;
+
+public interface MediaService {
+    String storePhoto(String filename) throws ServiceException;
+
+    String getPreviewPhoto(String publicId);
+
+    String getAlbumPhoto(String publicId);
+}
