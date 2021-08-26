918d494308aa0f91787176e7ec760112d9abcec5
alexridvansky <alexridvansky@gmail.com>
2021-08-26 16:40:26 +0300

    ConnectionPoolTest - @aftertest added

--- src/test/java/by/spetr/web/model/pool/ConnectionPoolTest.java
+++ src/test/java/by/spetr/web/model/pool/ConnectionPoolTest.java
@@ -2,6 +2,7 @@ package by.spetr.web.model.pool;
 
 import by.spetr.web.model.exception.ConnectionPoolException;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -35,5 +36,10 @@ public class ConnectionPoolTest {
                 connection.close();
             }
         }
+    }
+
+    @AfterMethod
+    public void tearDown() throws ConnectionPoolException {
+        connectionPool.destroyPool();
     }
 }
