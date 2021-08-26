8e854ea4cd5b5c8b2c6d357be4e4bed4dd886019
alexridvansky <alexridvansky@gmail.com>
2021-08-26 16:16:35 +0300

    ConnectionPoolTest - test added

---
+++ src/test/java/by/spetr/web/model/pool/ConnectionPoolTest.java
@@ -0,0 +1,40 @@
+package by.spetr.web.model.pool;
+
+import by.spetr.web.model.exception.ConnectionPoolException;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectionPoolTest {
+    private static final int EXPECTED_POOL_SIZE = 8;
+    private static ConnectionPool connectionPool;
+
+    @BeforeMethod
+    public void setUp() {
+        connectionPool = ConnectionPool.getInstance();
+    }
+
+    @Test
+    public void testGetConnection() throws ConnectionPoolException, SQLException {
+        List<Connection> connections = new ArrayList<>();
+
+        try {
+            for (int i = 0; i < 8; i++) {
+                Connection connection = connectionPool.getConnection();
+                connections.add(connection);
+            }
+
+            Assert.assertEquals(connections.size(), EXPECTED_POOL_SIZE);
+        } finally {
+            for (int i = 0; i < connections.size(); i++) {
+                connections.get(i).close();
+            }
+        };
+    }
+}
