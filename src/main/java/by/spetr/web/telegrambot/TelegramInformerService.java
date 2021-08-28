5afc037d67a265ce1fd732dafa891a0f41e361f4
alexridvansky <alexridvansky@gmail.com>
2021-08-28 12:00:06 +0300

    DefaultUserDao - patch fix

--- src/main/java/by/spetr/web/telegrambot/TelegramInformerService.java
+++ src/main/java/by/spetr/web/telegrambot/TelegramInformerService.java
@@ -22,15 +22,11 @@ import java.util.List;
 public final class TelegramInformerService extends TelegramLongPollingBot implements InformerService{
     private static final Logger logger = LogManager.getLogger();
     private static final UserService userService = UserService.getInstance();
-    // comment about placing tokens, sql-connection passwords and all that stuff into property and pushing to the git:
-    // I know that it isn't the best way now to deal with it but it's the easiest way
-    // how to share it with you. In a real situation i'd put it to the server's or vm's local variables
-    private static final String BOT_TOKEN_PROPERTY = "telegram.bot_token";
     private static final String BOT_NAME_PROPERTY = "telegram.bot_name";
     private static final String BOT_CHAT_ID_PROPERTY = "telegram.chat_id";
     private static final String MAX_ATT_COUNT_PROPERTY = "telegram.confirm_count";
     private static final String ATT_EXPIRES_TIME_PROPERTY = "telegram.confirm_expires";
-    private static final String BOT_TOKEN;
+    private static final String BOT_TOKEN = System.getenv("BOT_TOKEN");
     private static final String BOT_NAME;
     private static final String BOT_CHAT_ID;
     private static final int MAX_ATT_COUNT;
@@ -38,7 +34,6 @@ public final class TelegramInformerService extends TelegramLongPollingBot implem
     private static TelegramInformerService instance;
 
     static {
-        BOT_TOKEN = PropertyReader.getInstance().getBotProperty(BOT_TOKEN_PROPERTY);
         BOT_NAME = PropertyReader.getInstance().getBotProperty(BOT_NAME_PROPERTY);
         BOT_CHAT_ID = PropertyReader.getInstance().getBotProperty(BOT_CHAT_ID_PROPERTY);
         MAX_ATT_COUNT = Integer.parseInt(PropertyReader.getInstance().getBotProperty(MAX_ATT_COUNT_PROPERTY));
